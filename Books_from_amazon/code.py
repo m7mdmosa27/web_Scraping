069d9acd8f9ad89c5dda51563cd789b01be02782
Mohamed Mosa <61886289+m7mdmosa27@users.noreply.github.com>
2022-07-04 20:12:44 +0200

    Create code.py

---
+++ Books_from_amazon/code.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import csv
+
+driver = webdriver.Chrome(r"E:\ITI AI-Pro Track\data perperation\chromedriver_win32\chromedriver.exe")
+driver.set_script_timeout(5000)
+driver.get("https://www.amazon.in/gp/bestsellers/books/")
+nameofbook = driver.find_elements(by="xpath", value="//div[@class='p13n-sc-truncated']")
+priceofbook = driver.find_elements(by="xpath", value="//span[@class='p13n-sc-price']")
+
+# print(len(nameofbook), len(priceofbook))
+
+for i in range(len(nameofbook)):
+    print(nameofbook[i].text, priceofbook[i].text)
+
+header = ["name of book", 'price']
+with open('booksDatasetAmazon.csv', 'w', encoding='UTF8', newline='') as filecsv:
+    writer = csv.writer(filecsv, delimiter=',')
+    writer.writerow(header)
+    for i in range(len(nameofbook)):
+        writer.writerow([nameofbook[i].text, priceofbook[i].text])
+
+
+
+driver.close()
